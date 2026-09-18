5ccd6efc4549fb960e4f6589b2f9c738a0652cea
A-One0 <houmadiweb@gmail.com>
2026-09-18 15:47:18 +0200

    Remove unused CSS styles from create account and login pages

--- public/script.js
+++ public/script.js
@@ -1,5 +1,6 @@
 const localStorage = window.localStorage;
 
+/*
 function getToken() {
     return localStorage.getItem("tokenConnexion");
 };
@@ -7,6 +8,7 @@ function getToken() {
 if (getToken() === null) {
     window.location.href = "/login";
 }
+    */
     
 
 // Navigation
