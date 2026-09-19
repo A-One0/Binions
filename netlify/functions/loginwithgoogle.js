322967adc8051c982dd3992e5afa8165237ce751
A-One0 <houmadiweb@gmail.com>
2026-09-19 12:01:39 +0200

    change funcgoogle login

--- netlify/functions/loginwithgoogle.js
+++ netlify/functions/loginwithgoogle.js
@@ -1,5 +1,5 @@
-const { signInWithGoogle } = require("/src/server/db.js");
-const config = require("/src/server/config.json")
+const { signInWithGoogle } = require("../../src/server/db.js");
+const config = require("../../src/server/config.json")
 
 exports.handler = async (event, context) => {
     const plr = await signInWithGoogle();
