058eb3a16deb9d567dbf83275614ef2df42a99f2
A-One0 <houmadiweb@gmail.com>
2026-09-19 11:52:08 +0200

    t2

--- netlify/functions/loginwithgoogle.js
+++ netlify/functions/loginwithgoogle.js
@@ -4,5 +4,8 @@ const config = require("/src/server/config.json")
 exports.handler = async (event, context) => {
     const plr = await signInWithGoogle();
     
-
+    return {
+        statusCode: 200,
+        body: JSON.stringify({ message: "Successfully signed in with Google", player: plr }),
+    };
 }
