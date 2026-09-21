6a6a3735c38c7c074c8695ae7f88c1741c217950
A-One0 <houmadiweb@gmail.com>
2026-09-21 10:54:47 +0200

    vercel deploy v2

--- netlify/functions/loginwithgoogle.js
+++ netlify/functions/loginwithgoogle.js
@@ -1,11 +1,7 @@
 const { signInWithGoogle } = require("../../src/server/db.js");
-const config = require("../../src/server/config.json")
 
-exports.handler = async (event, context) => {
-    const plr = await signInWithGoogle();
+export default async function handler(req, res) {
+  const plr = await signInWithGoogle();
     
-    return {
-        statusCode: 200,
-        body: JSON.stringify({ message: "Successfully signed in with Google", player: plr }),
-    };
+   res.status(200).json(data);
 }
