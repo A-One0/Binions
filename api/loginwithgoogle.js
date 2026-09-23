bcb2bc278c76564eb996218ea4f5b9f556bd0330
binionscontact-collab <binions.contact@gmail.com>
2026-09-23 11:47:09 +0200

    modif loginwithgoogle

--- api/loginwithgoogle.js
+++ api/loginwithgoogle.js
@@ -1,19 +1,17 @@
 import { createDocument } from "../src/server/db.js";
 import { createToken } from "../src/server/userfind/createtoken.js";
-import config from "../src/server/config.json" assert { type: "json" };
+import config from "../src/server/config.json" with { type: "json" };
 
 export default async function handler(req, res) {
   const { user, name } = req.body;
-
   const token = createToken();
 
-
-  const data = config.plrNull;
+  const data = { ...config.plrNull };
   data.methodConnexion = "google";
-  data.username = name 
+  data.username = name;
   data.actualtoken = token;
 
   await createDocument(config.dbCollectionPlayer, user.uid, data);
-    
-   res.status(200).json(token);
+
+  res.status(200).json(token);
 }
